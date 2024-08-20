5506d055ea7fc2654ac192b7d4b4d22ef0fa7d4b
Hector <hectwilliams@gmail.com>
2024-08-20 00:30:10 -0400

    vectoer3d operations

--- vector3D.py
+++ vector3D.py
@@ -3,39 +3,69 @@ import multiprocessing.managers
 import multiprocessing.process
 from decimal import Decimal
 from math import sqrt
-import time 
-import os 
-import asyncio 
-import py_compile
-import hashlib 
 import numbers 
 
 IP_ADDRESS='127.0.0.1'
 PORT=50000
 AUTHKEY=b'abcd'
 
-class Vector3:
+class Vector3():
     def __init__(self,x=0, y=0, z=0):
         self.x=x
         self.y=y
         self.z=z
-    def getxyz(self):
-        return (self.x, self.y, self.z) 
-    def setxyz(self, value):
-        self.x=value[0]
-        self.y=value[1]
-        self.z=value[2]
-    vector=property(getxyz,setxyz)
-    def __repr__(self):
+        self._type = Vector3
+    def get(self):
+        return self
+    def set(self, vector3):
+        for attr, _ in self.__dict__.items():
+            self.__dict__[attr]=vector3.__dict__[attr]
+    vector=property(get,set)
+    def __repr__(self) -> str:
         return f'Vector3( x = { self.x }, y = { self.y }, z = { self.z } )'
+    def zero(self):
+        for attr, _ in self.__dict__.items():
+            self.__dict__[attr]=0
     def __mul__(self, value):
-        return Vector3(self.x * value, self.y * value, self.z * value)
+        if isinstance(value, numbers.Number):
+            return Vector3(self.x * value, self.y * value, self.z * value)
+        if isinstance(value, Vector3):
+            # dot product
+            print("dot product")
+            return self.x * value.x + self.y * value.y + self.z * value.z
+        raise TypeError("Vector3 or scalar are permitted")
     def __rmul__(self, val):
         return self.__mul__(val)
     def __add__(self, value):
-        return Vector3(self.x + value, self.y + value, self.z + value)
+        if isinstance(value, numbers.Number):
+            return Vector3(self.x + value, self.y + value, self.z + value)
+        if isinstance(value, Vector3):
+            return Vector3(self.x + value.x, self.y + value.y, self.z + value.z)
+        raise TypeError("Vector3 or scalar are permitted")
     def __sub__(self, value):
-        return Vector3(self.x - value, self.y - value, self.z - value)
+        if isinstance(value, numbers.Number):
+            return Vector3(self.x - value, self.y - value, self.z - value)
+        if isinstance(value, Vector3):
+            return Vector3(self.x - value.x, self.y - value.y, self.z - value.z)
+        raise TypeError("Vector3 or scalar are permitted")
     def __truediv__(self, value):
-        return Vector3(self.x/value, self.y/value, self.z/value)
-            
+        try:
+            if isinstance(value, numbers.Number):
+                return Vector3(self.x/value, self.y/value, self.z/value)
+            if isinstance(value, Vector3):
+                return Vector3(self.x / value.x, self.y / value.y, self.z / value.z)
+            raise TypeError("Vector3 or scalar are permitted")
+        except ZeroDivisionError as e:
+            print("Divide by Zero Error")
+    def __eq__(self, vector3) -> bool:
+        return all([value == vector3.__dict__[attr] for attr,value in self.__dict__.items()] )
+    def normalize(self) :
+        print(self)
+        mag_sq = self.x**2 + self.y**2 + self.z**2
+        if mag_sq > 0:
+            one_over_sqrt_mag = 1 / sqrt(mag_sq)
+            print(one_over_sqrt_mag)
+            self.x *= one_over_sqrt_mag
+            self.y *= one_over_sqrt_mag
+            self.z *= one_over_sqrt_mag
+
