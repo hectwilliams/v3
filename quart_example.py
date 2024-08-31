43bbc227a1198c13d6f4e9724f2e1f49b6555e7e
Hector <hectwilliams@gmail.com>
2024-08-31 17:07:44 -0400

    use slerp to interpolate rotation

--- quart_example.py
+++ quart_example.py
@@ -23,13 +23,13 @@ async def rotate_cardinal(axes):
     await asyncio.sleep(0.5)
     v = vector3.Vector3(0, 1, 1)
     m = matrix_4x3.Matrix4x3()
-    theta = (30/360.0)*math.pi
+    theta = (60/360.0)*math.pi
     q = quarternion.Quarternion()
+    q.set_to_rotate_about_x(theta)
+    m.from_quarternion(q)
     while True:
         plt.pause(0.1)
         axes.scatter(v.x, v.y, v.z, marker='o', c='blue', s=1)
-        q.set_to_rotate_about_x(theta)
-        m.from_quarternion(q)
         v = matrix_4x3.vector_mult(v, m)
         await asyncio.sleep(0.0)
 async def rotate_arbitrary_axis(axes, rng):
@@ -40,7 +40,6 @@ async def rotate_arbitrary_axis(axes, rng):
     theta = (30/360.0)*math.pi
     m = matrix_4x3.Matrix4x3()
     n = vector3.Vector3( abs(rng.normal() ) , abs(rng.normal()) , rng.normal())
-    print(n)
     n.normalize()
     q.set_to_rotate_about_axis(n, theta)
     m.from_quarternion(q)
@@ -50,7 +49,22 @@ async def rotate_arbitrary_axis(axes, rng):
         plt.pause(0.1)
         print(v_start*v)
         await asyncio.sleep(0.0)
-
+async def rotate_cardinal_2(axes):
+    """Use sleep to interpolate rotation"""
+    q1 = quarternion.Quarternion()
+    q2 = quarternion.Quarternion()
+    q1.set_to_rotate_about_x(0.0)
+    q2.set_to_rotate_about_x( (60/360.0) * math.pi )
+    m = matrix_4x3.Matrix4x3()
+    m.from_quarternion(q1)
+    q = quarternion.slerp(q1, q2, 0.5)
+    m.from_quarternion(q)
+    v =vector3.Vector3(0, 1, 1)
+    while True:
+        v = matrix_4x3.vector_mult(v, m)
+        axes.scatter(v.x, v.y, v.z, marker='*', c='green', s=2)
+        plt.pause(0.1)
+        await asyncio.sleep(0.1)
 rng = np.random.Generator(np.random.PCG64(242))
 fig = plt.figure() 
 ax = fig.add_subplot(projection='3d')
@@ -65,6 +79,7 @@ plt.show()
 loop = asyncio.new_event_loop() 
 t1 = loop.create_task(draw_x_line(ax))
 t2 = loop.create_task(rotate_cardinal(ax))
+t4= loop.create_task(rotate_cardinal_2(ax))
 t3 = loop.create_task(rotate_arbitrary_axis(ax, rng))
 loop.run_forever()
 
